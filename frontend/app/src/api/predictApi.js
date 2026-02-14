75bdd6052c916ac4a349d82a1eb9f4157eb378a2
Aditya Sharma <sharmadi.kota@gmail.com>
2026-02-14 12:39:11 +0530

    Update predictApi.js

--- frontend/app/src/api/predictApi.js
+++ frontend/app/src/api/predictApi.js
@@ -1,5 +1,5 @@
 export const predictCOPD = async (patientData) => {
-  const response = await fetch("http://127.0.0.1:8000/predict", {
+  const response = await fetch("https://copd-fullstack.onrender.com/predict", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
